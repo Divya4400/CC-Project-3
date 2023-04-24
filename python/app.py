2191f971d9cc535e04a93f8ac6ac2d530095c4cb
Joyce Horn <hornjoyce@gmail.com>
2023-04-24 17:41:04 -0600

    Adds middleware to allow from cross origin validation in order to be able to test on browser without getting CORS error

--- python/app.py
+++ python/app.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from middleware import add_middleware
+
 
 app = FastAPI()
+
+# add middleware
+add_middleware(app)
 
 class Recommendation(BaseModel):
     description: str
